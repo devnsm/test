a87cd7f62e0230b5d23e8730d6b072c709181315
mrtv66 <66mrtv@gmail.com>
2023-02-06 15:17:42 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+async function getData() {
+  const request = await axios.get("https://jsonplaceholder.typicode.com/todos");
+  const response = await request.data;
+
+  console.log(response);
+}
+
+getData();
